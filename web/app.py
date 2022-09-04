ad20c0a2b866d9a23c7057ca25e4f9fa754abc05
Eric Caron <eric.caron@gmail.com>
2022-09-04 15:37:06 -0500

    Interface now saves & retrieves & edits all pattern adjustments

--- web/app.py
+++ web/app.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template, request, redirect, send_from_directory
 import configparser
 import json
 import adafruit_led_animation
-config = configparser.ConfigParser()
-config.read('settings.conf')
+
+config = configparser.ConfigParser(strict=False, interpolation=None)
+settings_conf = os.getenv('SETTINGS_CONF')
+if not settings_conf:
+    settings_conf = '/tmp/settings.conf'
+config.read(settings_conf)
+
 app = Flask(__name__)
 
 @app.route('/button/<number>', methods=['GET'])
@@ -20,27 +25,39 @@ def favicon():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        config['TIMERS']['TurnOn'] = request.form['turn_on']
-        config['TIMERS']['TurnOff'] = request.form['turn_off']
-        config['TIMERS']['AutoOff'] = request.form['auto_off']
-        with open('settings.conf', 'w') as configfile:
-            config.write(configfile)
-        return redirect("/", code=302)
-    else:
-        patterns = []
-        for key in config['PATTERNS']:
-            patterns.append(json.loads(config['PATTERNS'][key]))
-        return render_template('index.html', timers=config['TIMERS'], patterns=patterns)
+        if request.form['method'] == 'save-patterns':
+            if 'PATTERNS' not in config:
+                config['PATTERNS'] = {}
 
-@app.route('/patterns', methods=['POST'])
-def patterns():
-    #Get inspiration from https://github.com/sanc909/C.H.I.P.-NeoPixel-SPI-Control-Visualiser/blob/master/ChipNeoController.py
-    config['PATTERNS'] = {}
-    for x in range(request.form['count']):
-        config['PATTERNS']['pattern', x] = x
-        with open('settings.conf', 'w') as configfile:
+            config['PATTERNS']['designs'] = request.form['patterns']
+        else:
+            if 'TIMERS' not in config:
+                config['TIMERS'] = {}
+
+            config['TIMERS']['TurnOn'] = request.form['turn_on']
+            config['TIMERS']['TurnOff'] = request.form['turn_off']
+            config['TIMERS']['AutoOff'] = request.form['auto_off']
+        with open(settings_conf, 'w') as configfile:
             config.write(configfile)
-        return redirect("/", code=302)
+
+        if request.form['method'] == 'save-patterns':
+            return '', 204
+        else:
+            return redirect("/", code=302)
+    else:
+        if 'PATTERNS' in config:
+            patterns = json.loads(config['PATTERNS']['designs'])
+        else:
+            patterns = []
+
+        if 'TIMERS' in config:
+            timers = config['TIMERS']
+        else:
+            timers = {}
+            timers['TurnOn'] = '16:00'
+            timers['TurnOff'] = '22:00'
+            timers['AutoOff'] = '4'
+        return render_template('index.html', timers=timers, patterns=patterns)
 
 if __name__ == '__main__':
     app.run(debug=True)
