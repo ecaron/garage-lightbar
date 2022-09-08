d55dc1878fd0eab3934b9ef3acf6d22f0c9127ff
Eric Caron <eric.caron@gmail.com>
2022-09-08 08:31:58 -0500

    Pass pylint

--- web/app.py
+++ web/app.py
@@ -1,31 +1,36 @@
+'''Main Application'''
 import os
 import configparser
 import json
-if not os.getenv('SKIP_LIGHTS'):
-    import controls
-from multiprocessing import Process, Value, Lock, Array
-from flask import Flask, render_template, request, redirect, send_from_directory
-from threading import Timer
 import datetime
+from multiprocessing import Process, Value, Lock, Array
+from threading import Timer
+from flask import Flask, render_template, request, redirect, send_from_directory
+if not os.getenv("SKIP_LIGHTS"):
+    import controls
+if not os.getenv("SKIP_BUTTONS"):
+    from gpiozero import Button # pylint: disable=import-error
 
-if not os.getenv('SKIP_BUTTONS'):
-    from gpiozero import Button
-
-config = configparser.ConfigParser(strict=False, interpolation=None)
-settings_conf = os.getenv('SETTINGS_CONF')
-if not settings_conf:
-    settings_conf = '/tmp/settings.conf'
-config.read(settings_conf)
+CONFIG = configparser.ConfigParser(strict=False, interpolation=None)
+SETTINGS_CONF = os.getenv("SETTINGS_CONF")
+if not SETTINGS_CONF:
+    SETTINGS_CONF = "/tmp/settings.conf"
+CONFIG.read(SETTINGS_CONF)
 
 app = Flask(__name__)
 
-class LightState(object):
+
+class LightState():
+    '''Controller of the light state settings'''
     def __init__(self):
-        self.pattern = Array('c', b'hello world')
-        self.power_on = Value('i', 0)
+        '''Initializes the shared memory'''
+        self.pattern = Array("c", b"hello world")
+        self.brightness = Value("i", 0)
+        self.power_on = Value("i", 0)
         self.lock = Lock()
 
     def toggle_power(self):
+        '''Gets the shared-memory power state'''
         with self.lock:
             if self.power_on.value == 0:
                 self.power_on.value = 1
@@ -33,38 +38,121 @@ class LightState(object):
                 self.power_on.value = 0
 
     def get_power(self):
+        '''Get the shared-memory power state'''
         with self.lock:
             return self.power_on.value
 
+    def get_brightness(self):
+        '''Gets the shared-memory brightness'''
+        with self.lock:
+            return self.brightness.value
+
+    def adjust_brightness(self):
+        '''Toggles the shared-memory brightness'''
+        with self.lock:
+            if self.brightness.value * 2 > 100:
+                self.brightness.value = 5
+            else:
+                self.brightness.value = self.brightness.value * 2
+
     def set_pattern(self, pattern):
+        '''Toggles the shared-memory pattern'''
         with self.lock:
             self.pattern.value = pattern
 
-ranToday = False
 
-def dailyReset():
-    global ranToday
-    ranToday = False
-    tomorrow = datetime.datetime.combine(datetime.date.today() + datetime.timedelta(days=1), datetime.time(0))
-    now = datetime.datetime.now()
-    diff = tomorrow - now
-    print (diff.total_seconds())
-    newDayTimer = Timer(diff.total_seconds(), dailyReset)
-    newDayTimer.start()
+PRESET_SPOT = 0
+DAILY_TIMER = False
+TURN_ON_TIMER = False
+TURN_OFF_TIMER = False
 
-Timer(1.0, dailyReset).start()
+def daily_reset():
+    '''Every morning, schedules the time that the lightbar should turn on'''
+    global DAILY_TIMER, TURN_ON_TIMER # pylint: disable=global-statement
 
-lightState = LightState()
+    if DAILY_TIMER:
+        DAILY_TIMER.cancel()
+
+    time_tomorrow = datetime.datetime.combine(
+        datetime.date.today() + datetime.timedelta(days=1), datetime.time(0)
+    )
+    time_now = datetime.datetime.now()
+    time_diff = time_tomorrow - time_now
+
+    DAILY_TIMER = Timer(time_diff.total_seconds(), daily_reset)
+    DAILY_TIMER.start()
+
+    if CONFIG["TIMERS"] and CONFIG["TIMERS"]["TurnOn"]:
+        turn_on_time = datetime.datetime.strptime(
+            CONFIG["TIMERS"]["TurnOn"], "%H:%M"
+        ).time()
+        turn_on_date_time = datetime.datetime.combine(datetime.date.today(), turn_on_time)
+        if TURN_ON_TIMER:
+            TURN_ON_TIMER.cancel()
+        TURN_ON_TIMER = Timer((turn_on_date_time - time_now).total_seconds(), turn_on)
+        TURN_ON_TIMER.start()
+
+
+def turn_on():
+    '''Turns the lightbar on'''
+    global TURN_OFF_TIMER # pylint: disable=global-statement
+    if LIGHT_STATE.get_power() == 0:
+        LIGHT_STATE.toggle_power()
+    if TURN_OFF_TIMER:
+        TURN_OFF_TIMER.cancel()
+    time_now = datetime.datetime.now()
+    turn_off_time = datetime.datetime.now() + datetime.timedelta(
+        hours=int(CONFIG["TIMERS"]["AutoOff"])
+    )
+    TURN_OFF_TIMER = Timer((turn_off_time - time_now).total_seconds(), turn_off)
+    TURN_OFF_TIMER.start()
+
+
+def turn_off():
+    '''Turns the lightbar off'''
+    if LIGHT_STATE.get_power() == 1:
+        LIGHT_STATE.toggle_power()
+
 
 def adjust_brightness():
-    return 1
+    '''Cycles the lightbar's brightness'''
+    LIGHT_STATE.adjust_brightness()
+
 
 def next_preset():
-    return 1
-def prev_preset():
+    '''Sends the next pattern to the lightbar'''
+    global PRESET_SPOT # pylint: disable=global-statement
+    patterns = json.loads(CONFIG["PATTERNS"]["designs"])
+    if PRESET_SPOT + 1 >= len(patterns):
+        PRESET_SPOT = 0
+    else:
+        PRESET_SPOT = PRESET_SPOT + 1
+    run_preset(patterns[PRESET_SPOT])
     return 1
 
-if not os.getenv('SKIP_BUTTONS'):
+
+def prev_preset():
+    '''Sends the previous pattern to the lightbar'''
+    global PRESET_SPOT # pylint: disable=global-statement
+    patterns = json.loads(CONFIG["PATTERNS"]["designs"])
+    if PRESET_SPOT <= 0:
+        PRESET_SPOT = len(patterns) - 1
+    else:
+        PRESET_SPOT = PRESET_SPOT - 1
+    run_preset(patterns[PRESET_SPOT])
+    return 1
+
+
+def run_preset(preset):
+    '''Sends a preset to the lightbar'''
+    print(preset)
+
+
+DAILY_TIMER = Timer(1.0, daily_reset)
+DAILY_TIMER.start()
+LIGHT_STATE = LightState()
+
+if not os.getenv("SKIP_BUTTONS"):
     button1 = Button(16)
     button1.when_released = next_preset
 
@@ -75,83 +163,92 @@ if not os.getenv('SKIP_BUTTONS'):
     button3.when_pressed = adjust_brightness
 
     button4 = Button(12)
-    button4.when_pressed = lightState.toggle_power
+    button4.when_pressed = LIGHT_STATE.toggle_power
 
-@app.route('/button/<number>', methods=['GET'])
+
+@app.route("/button/<number>", methods=["GET"])
 def button(number):
-    if number == '4':
-        lightState.toggle_power()
-    elif number == '3':
+    '''Handles when the virtual buttons are pushed'''
+    if number == "4":
+        LIGHT_STATE.toggle_power()
+    elif number == "3":
         adjust_brightness()
-    elif number == '2':
-        next_preset()
-    else:
+    elif number == "2":
         prev_preset()
-    return '', 204
+    else:
+        next_preset()
+    return "", 204
 
-@app.route('/favicon.ico')
+
+@app.route("/favicon.ico")
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
+    '''Returns a favicon'''
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        if request.form['method'] == 'run-pattern':
-            print (request.form['pattern'])
-            return '', 204
+    '''Serves & saves the web portal'''
+    if request.method == "POST":
+        if request.form["method"] == "run-pattern":
+            print(request.form["pattern"])
+            return "", 204
 
-        if request.form['method'] == 'save-patterns':
-            if 'PATTERNS' not in config:
-                config['PATTERNS'] = {}
+        if request.form["method"] == "save-patterns":
+            if "PATTERNS" not in CONFIG:
+                CONFIG["PATTERNS"] = {}
 
-            config['PATTERNS']['designs'] = request.form['patterns']
+            CONFIG["PATTERNS"]["designs"] = request.form["patterns"]
         else:
-            if 'TIMERS' not in config:
-                config['TIMERS'] = {}
+            if "TIMERS" not in CONFIG:
+                CONFIG["TIMERS"] = {}
 
-            config['TIMERS']['TurnOn'] = request.form['turn_on']
-            config['TIMERS']['AutoOff'] = request.form['auto_off']
-        with open(settings_conf, 'w') as configfile:
-            config.write(configfile)
+            CONFIG["TIMERS"]["TurnOn"] = request.form["turn_on"]
+            CONFIG["TIMERS"]["AutoOff"] = request.form["auto_off"]
+        with open(SETTINGS_CONF, mode="w", encoding="utf-8") as configfile:
+            CONFIG.write(configfile)
 
-        if request.form['method'] == 'save-patterns':
-            return '', 204
-        else:
-            return redirect("/", code=302)
+        if request.form["method"] == "save-patterns":
+            return "", 204
+        return redirect("/", code=302)
 
-    if 'PATTERNS' in config:
-        patterns = json.loads(config['PATTERNS']['designs'])
+    if "PATTERNS" in CONFIG:
+        patterns = json.loads(CONFIG["PATTERNS"]["designs"])
     else:
         patterns = []
 
-    if 'TIMERS' in config:
-        timers = config['TIMERS']
+    if "TIMERS" in CONFIG:
+        timers = CONFIG["TIMERS"]
     else:
         timers = {}
-        timers['TurnOn'] = '16:00'
-        timers['AutoOff'] = '4'
-    return render_template('index.html', timers=timers, patterns=patterns)
+        timers["TurnOn"] = "16:00"
+        timers["AutoOff"] = "4"
+    return render_template("index.html", timers=timers, patterns=patterns)
 
-if __name__ == '__main__':
-    host = '127.0.0.1'
-    if os.getenv('HOST'):
-        host = os.getenv('HOST')
-    port = 8000
-    if os.getenv('PORT'):
-        port = os.getenv('PORT')
-    debug = False
-    if os.getenv('DEBUG'):
-        debug = os.getenv('DEBUG')
-    reloader = False
-    if os.getenv('RELOADER'):
-        reloader = os.getenv('RELOADER')
 
-    if not os.getenv('SKIP_LIGHTS'):
-        p = Process(target=controls.lights, args=(lightState,))
+if __name__ == "__main__":
+    HOST = "127.0.0.1"
+    if os.getenv("HOST"):
+        HOST = os.getenv("HOST")
+    PORT = 8000
+    if os.getenv("PORT"):
+        PORT = os.getenv("PORT")
+    DEBUG = False
+    if os.getenv("DEBUG"):
+        DEBUG = os.getenv("DEBUG")
+    RELOADER = False
+    if os.getenv("RELOADER"):
+        RELOADER = os.getenv("RELOADER")
+
+    if not os.getenv("SKIP_LIGHTS"):
+        p = Process(target=controls.lights, args=(LIGHT_STATE,))
         p.start()
 
-    app.run(host=host,port=port,debug=debug, use_reloader=reloader)
+    app.run(host=HOST, port=PORT, debug=DEBUG, use_reloader=RELOADER)
 
-    if not os.getenv('SKIP_LIGHTS'):
+    if not os.getenv("SKIP_LIGHTS"):
         p.join()
--- web/controls.py
+++ web/controls.py
@@ -1,14 +1,15 @@
-import board
-import neopixel
+'''Light animation handler'''
 import time
+import board # pylint: disable=import-error
+import neopixel # pylint: disable=import-error
 from adafruit_led_animation.helper import PixelMap
 from adafruit_led_animation.animation.comet import Comet
-from adafruit_led_animation.animation.rainbowcomet import RainbowComet
-from adafruit_led_animation.animation.rainbowchase import RainbowChase
-from adafruit_led_animation.animation.chase import Chase
-from adafruit_led_animation.animation.rainbow import Rainbow
-from adafruit_led_animation.sequence import AnimationSequence
-from adafruit_led_animation import helper
+# from adafruit_led_animation.animation.rainbowcomet import RainbowComet
+# from adafruit_led_animation.animation.rainbowchase import RainbowChase
+# from adafruit_led_animation.animation.chase import Chase
+# from adafruit_led_animation.animation.rainbow import Rainbow
+# from adafruit_led_animation.sequence import AnimationSequence
+# from adafruit_led_animation import helper
 
 pixels = neopixel.NeoPixel(board.D18, 60, auto_write=False)
 blue_lights = [13, 14, 15, 16, 17, 18, 19, 20, 21]
@@ -26,17 +27,13 @@ white_map = PixelMap(pixels, white_lights, individual_pixels=True)
 red_lights = [47, 48, 49, 50, 51, 52, 53, 54, 55]
 red_map = PixelMap(pixels, red_lights, individual_pixels=True)
 
-power_on = True
-brightness_adjust = 1
-animation_spot = 0
+comet_h = Comet(white_map, speed=0.1, color=(255, 150, 0), tail_length=3, bounce=True)
 
-comet_h = Comet(
-    white_map, speed=0.1, color=(255, 150, 0), tail_length=3, bounce=True
-)
 
-def lights(lightState):
+def lights(light_state):
+    '''Animates, powers and alters the light state'''
     while True:
-        if lightState.get_power() == 1:
+        if light_state.get_power() == 1:
             comet_h.animate()
         else:
-            time.sleep(.2)
+            time.sleep(0.2)
