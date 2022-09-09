86af58ca1e4eec7c0c6643b0cf7e59ffdf25daa0
Eric Caron <eric.caron@gmail.com>
2022-09-09 05:33:19 +0100

    Better light management

--- web/app.py
+++ web/app.py
@@ -1,4 +1,4 @@
-'''Main Application'''
+"""Main Application"""
 import os
 import configparser
 import json
@@ -6,10 +6,11 @@ import datetime
 from multiprocessing import Process, Value, Lock, Array
 from threading import Timer
 from flask import Flask, render_template, request, redirect, send_from_directory
+
 if not os.getenv("SKIP_LIGHTS"):
     import controls
 if not os.getenv("SKIP_BUTTONS"):
-    from gpiozero import Button # pylint: disable=import-error
+    from gpiozero import Button  # pylint: disable=import-error
 
 CONFIG = configparser.ConfigParser(strict=False, interpolation=None)
 SETTINGS_CONF = os.getenv("SETTINGS_CONF")
@@ -28,17 +29,19 @@ if "TIMERS" not in CONFIG:
 
 app = Flask(__name__)
 
-class LightState():
-    '''Controller of the light state settings'''
+
+class LightState:
+    """Controller of the light state settings"""
+
     def __init__(self):
-        '''Initializes the shared memory'''
+        """Initializes the shared memory"""
         self.pattern = Array("c", bytearray(1024))
         self.brightness = Value("i", 0)
         self.power_on = Value("i", 0)
         self.lock = Lock()
 
     def toggle_power(self):
-        '''Gets the shared-memory power state'''
+        """Gets the shared-memory power state"""
         with self.lock:
             if self.power_on.value == 0:
                 self.power_on.value = 1
@@ -46,28 +49,32 @@ class LightState():
                 self.power_on.value = 0
 
     def get_power(self):
-        '''Get the shared-memory power state'''
+        """Get the shared-memory power state"""
         with self.lock:
             return self.power_on.value
 
-    def get_brightness(self):
-        '''Gets the shared-memory brightness'''
-        with self.lock:
-            return self.brightness.value
-
     def adjust_brightness(self):
-        '''Toggles the shared-memory brightness'''
+        """Toggles the shared-memory brightness"""
         with self.lock:
             if self.brightness.value * 2 > 100:
                 self.brightness.value = 5
             else:
                 self.brightness.value = self.brightness.value * 2
 
-    def set_pattern(self, pattern):
-        '''Toggles the shared-memory pattern'''
+    def get_brightness(self):
+        """Gets the shared-memory brightness"""
         with self.lock:
-            self.pattern.value = bytes(pattern, 'utf-8')
-            print(pattern)
+            return self.brightness.value
+
+    def set_pattern(self, pattern):
+        """Toggles the shared-memory pattern"""
+        with self.lock:
+            self.pattern.value = bytes(pattern, "utf-8")
+
+    def get_pattern(self):
+        """Gets the shared-memory pattern"""
+        with self.lock:
+            return self.pattern.value.decode("utf-8")
 
 
 PRESET_SPOT = 0
@@ -75,9 +82,10 @@ DAILY_TIMER = False
 TURN_ON_TIMER = False
 TURN_OFF_TIMER = False
 
+
 def daily_reset():
-    '''Every morning, schedules the time that the lightbar should turn on'''
-    global DAILY_TIMER, TURN_ON_TIMER # pylint: disable=global-statement
+    """Every morning, schedules the time that the lightbar should turn on"""
+    global DAILY_TIMER, TURN_ON_TIMER  # pylint: disable=global-statement
 
     if DAILY_TIMER:
         DAILY_TIMER.cancel()
@@ -95,7 +103,9 @@ def daily_reset():
         turn_on_time = datetime.datetime.strptime(
             CONFIG["TIMERS"]["TurnOn"], "%H:%M"
         ).time()
-        turn_on_date_time = datetime.datetime.combine(datetime.date.today(), turn_on_time)
+        turn_on_date_time = datetime.datetime.combine(
+            datetime.date.today(), turn_on_time
+        )
         if TURN_ON_TIMER:
             TURN_ON_TIMER.cancel()
         TURN_ON_TIMER = Timer((turn_on_date_time - time_now).total_seconds(), turn_on)
@@ -103,8 +113,8 @@ def daily_reset():
 
 
 def turn_on():
-    '''Turns the lightbar on'''
-    global TURN_OFF_TIMER # pylint: disable=global-statement
+    """Turns the lightbar on"""
+    global TURN_OFF_TIMER  # pylint: disable=global-statement
     if LIGHT_STATE.get_power() == 0:
         LIGHT_STATE.toggle_power()
     if TURN_OFF_TIMER:
@@ -118,19 +128,21 @@ def turn_on():
 
 
 def turn_off():
-    '''Turns the lightbar off'''
+    """Turns the lightbar off"""
     if LIGHT_STATE.get_power() == 1:
         LIGHT_STATE.toggle_power()
 
 
 def adjust_brightness():
-    '''Cycles the lightbar's brightness'''
+    """Cycles the lightbar's brightness"""
     LIGHT_STATE.adjust_brightness()
 
 
 def next_preset():
-    '''Sends the next pattern to the lightbar'''
-    global PRESET_SPOT # pylint: disable=global-statement
+    """Sends the next pattern to the lightbar"""
+    global PRESET_SPOT  # pylint: disable=global-statement
+    if LIGHT_STATE.get_power() == 0:
+        LIGHT_STATE.toggle_power()
     patterns = json.loads(CONFIG["PATTERNS"]["designs"])
     if PRESET_SPOT + 1 >= len(patterns):
         PRESET_SPOT = 0
@@ -141,8 +153,10 @@ def next_preset():
 
 
 def prev_preset():
-    '''Sends the previous pattern to the lightbar'''
-    global PRESET_SPOT # pylint: disable=global-statement
+    """Sends the previous pattern to the lightbar"""
+    global PRESET_SPOT  # pylint: disable=global-statement
+    if LIGHT_STATE.get_power() == 0:
+        LIGHT_STATE.toggle_power()
     patterns = json.loads(CONFIG["PATTERNS"]["designs"])
     if PRESET_SPOT <= 0:
         PRESET_SPOT = len(patterns) - 1
@@ -153,7 +167,9 @@ def prev_preset():
 
 
 def run_preset(preset):
-    '''Sends a preset to the lightbar'''
+    """Sends a preset to the lightbar"""
+    if LIGHT_STATE.get_power() == 0:
+        LIGHT_STATE.toggle_power()
     LIGHT_STATE.set_pattern(preset)
 
 
@@ -177,7 +193,7 @@ if not os.getenv("SKIP_BUTTONS"):
 
 @app.route("/button/<number>", methods=["GET"])
 def button(number):
-    '''Handles when the virtual buttons are pushed'''
+    """Handles when the virtual buttons are pushed"""
     if number == "4":
         LIGHT_STATE.toggle_power()
     elif number == "3":
@@ -191,7 +207,7 @@ def button(number):
 
 @app.route("/favicon.ico")
 def favicon():
-    '''Returns a favicon'''
+    """Returns a favicon"""
     return send_from_directory(
         os.path.join(app.root_path, "static"),
         "favicon.ico",
@@ -201,7 +217,7 @@ def favicon():
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    '''Serves & saves the web portal'''
+    """Serves & saves the web portal"""
     if request.method == "POST":
         if request.form["method"] == "run-pattern":
             run_preset(request.form["pattern"])
--- web/controls.py
+++ web/controls.py
@@ -1,13 +1,20 @@
-'''Light animation handler'''
+"""Light animation handler"""
 import time
-import board # pylint: disable=import-error
-import neopixel # pylint: disable=import-error
+import board  # pylint: disable=import-error
+import neopixel  # pylint: disable=import-error
+from urllib.parse import parse_qs  # pylint: disable=wrong-import-order
 from adafruit_led_animation.helper import PixelMap
+from adafruit_led_animation.animation.blink import Blink
+from adafruit_led_animation.animation.chase import Chase
+from adafruit_led_animation.animation.colorcycle import ColorCycle
 from adafruit_led_animation.animation.comet import Comet
-# from adafruit_led_animation.animation.rainbowcomet import RainbowComet
-# from adafruit_led_animation.animation.rainbowchase import RainbowChase
-# from adafruit_led_animation.animation.chase import Chase
-# from adafruit_led_animation.animation.rainbow import Rainbow
+from adafruit_led_animation.animation.pulse import Pulse
+from adafruit_led_animation.animation.rainbowcomet import RainbowComet
+from adafruit_led_animation.animation.rainbowchase import RainbowChase
+from adafruit_led_animation.animation.rainbowsparkle import RainbowSparkle
+from adafruit_led_animation.animation.rainbow import Rainbow
+from adafruit_led_animation.color import WHITE
+
 # from adafruit_led_animation.sequence import AnimationSequence
 # from adafruit_led_animation import helper
 
@@ -27,13 +34,90 @@ white_map = PixelMap(pixels, white_lights, individual_pixels=True)
 red_lights = [47, 48, 49, 50, 51, 52, 53, 54, 55]
 red_map = PixelMap(pixels, red_lights, individual_pixels=True)
 
-comet_h = Comet(white_map, speed=0.1, color=(255, 150, 0), tail_length=3, bounce=True)
+PREV_POWER = 0
+PREV_BRIGHTNESS = 0
+PREV_PATTERN = ""
 
 
-def lights(light_state):
-    '''Animates, powers and alters the light state'''
-    while True:
+def lights(light_state): # pylint: disable=too-many-branches,too-many-statements
+    """Animates, powers and alters the light state"""
+    global PREV_POWER, PREV_BRIGHTNESS, PREV_PATTERN # pylint: disable=global-statement
+    pattern = Blink(white_map, speed=1.0, color=(0, 0, 0))
+    while True: # pylint: disable=too-many-nested-blocks
         if light_state.get_power() == 1:
-            comet_h.animate()
+            if PREV_POWER == 0:
+                PREV_POWER = 1
+            if (
+                PREV_PATTERN != light_state.get_pattern()
+                or PREV_BRIGHTNESS != light_state.get_brightness
+            ):
+                PREV_PATTERN = light_state.get_pattern()
+                PREV_BRIGHTNESS = light_state.get_brightness()
+                new_pattern = parse_qs(PREV_PATTERN)
+                if new_pattern["speed"][0] == "slowest":
+                    speed = 1
+                elif new_pattern["speed"][0] == "slow":
+                    speed = 0.8
+                elif new_pattern["speed"][0] == "normal":
+                    speed = 0.4
+                elif new_pattern["speed"][0] == "fast":
+                    speed = 0.2
+                elif new_pattern["speed"][0] == "fastest":
+                    speed = 0.1
+
+                tuple_color = WHITE
+                if "color" in new_pattern and len(new_pattern["color"]) == 1:
+                    hex_color = new_pattern["color"][0].lstrip("#")
+                    tuple_color = tuple(int(hex_color[i : i + 2], 16) for i in (0, 2, 4))
+
+                if new_pattern["zone"][0] == "rb-all":
+                    new_map = PixelMap(
+                        pixels, blue_lights + red_lights, individual_pixels=True
+                    )
+                elif new_pattern["zone"][0] == "white-all":
+                    new_map = PixelMap(pixels, white_lights, individual_pixels=True)
+                elif new_pattern["zone"][0] == "all":
+                    new_map = PixelMap(
+                        pixels,
+                        blue_lights + white_lights + red_lights,
+                        individual_pixels=True,
+                    )
+
+                if new_pattern["pattern"][0] == "blink":
+                    pattern = Blink(new_map, speed=speed, color=tuple_color)
+                elif new_pattern["pattern"][0] == "chase":
+                    pattern = Chase(new_map, speed=speed, color=tuple_color)
+                elif new_pattern["pattern"][0] == "colorcycle":
+                    if "colors" in new_pattern and len(new_pattern["colors"]) > 0:
+                        colors = []
+                        for hex_color in new_pattern["colors"]:
+                            hex_color = hex_color.lstrip("#")
+                            colors.append(
+                                tuple(int(hex_color[i : i + 2], 16) for i in (0, 2, 4))
+                            )
+                        pattern = ColorCycle(new_map, speed=speed, colors=colors)
+                    else:
+                        pattern = ColorCycle(new_map, speed=speed)
+                elif new_pattern["pattern"][0] == "comet":
+                    pattern = Comet(new_map, speed=speed, color=tuple_color)
+                elif new_pattern["pattern"][0] == "pulse":
+                    pattern = Pulse(new_map, speed=speed, color=tuple_color)
+                elif new_pattern["pattern"][0] == "rainbow":
+                    pattern = Rainbow(new_map, speed=speed)
+                elif new_pattern["pattern"][0] == "rainbowchase":
+                    pattern = RainbowChase(new_map, speed=speed)
+                elif new_pattern["pattern"][0] == "rainbowcomet":
+                    pattern = RainbowComet(new_map, speed=speed)
+                elif new_pattern["pattern"][0] == "rainbowsparkle":
+                    pattern = RainbowSparkle(new_map, speed=speed)
+
+                pixels.fill((0, 0, 0))
+                pixels.show()
+
+            pattern.animate()
         else:
+            if PREV_POWER == 1:
+                pixels.fill((0, 0, 0))
+                pixels.show()
+                PREV_POWER = 0
             time.sleep(0.2)
