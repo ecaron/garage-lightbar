85a9569fe8cdd2b40f87667e360207c95a68a6a8
Eric Caron <eric.caron@gmail.com>
2022-09-09 05:52:59 +0100

    Closer to functional

--- web/controls.py
+++ web/controls.py
@@ -39,21 +39,22 @@ PREV_BRIGHTNESS = 0
 PREV_PATTERN = ""
 
 
-def lights(light_state): # pylint: disable=too-many-branches,too-many-statements
+def lights(light_state):  # pylint: disable=too-many-branches,too-many-statements
     """Animates, powers and alters the light state"""
-    global PREV_POWER, PREV_BRIGHTNESS, PREV_PATTERN # pylint: disable=global-statement
+    global PREV_POWER, PREV_BRIGHTNESS, PREV_PATTERN  # pylint: disable=global-statement
     pattern = Blink(white_map, speed=1.0, color=(0, 0, 0))
-    while True: # pylint: disable=too-many-nested-blocks
+    while True:  # pylint: disable=too-many-nested-blocks
         if light_state.get_power() == 1:
             if PREV_POWER == 0:
                 PREV_POWER = 1
             if (
                 PREV_PATTERN != light_state.get_pattern()
-                or PREV_BRIGHTNESS != light_state.get_brightness
+                or PREV_BRIGHTNESS != light_state.get_brightness()
             ):
                 PREV_PATTERN = light_state.get_pattern()
                 PREV_BRIGHTNESS = light_state.get_brightness()
                 new_pattern = parse_qs(PREV_PATTERN)
+
                 if new_pattern["speed"][0] == "slowest":
                     speed = 1
                 elif new_pattern["speed"][0] == "slow":
@@ -68,12 +69,23 @@ def lights(light_state): # pylint: disable=too-many-branches,too-many-statements
                 tuple_color = WHITE
                 if "color" in new_pattern and len(new_pattern["color"]) == 1:
                     hex_color = new_pattern["color"][0].lstrip("#")
-                    tuple_color = tuple(int(hex_color[i : i + 2], 16) for i in (0, 2, 4))
+                    tuple_color = tuple(
+                        int(hex_color[i : i + 2], 16) for i in (0, 2, 4)
+                    )
 
+                # Zones: all, rwb-toggle, white-all, white-toggle, white-unique,
+                # rb-toggle, rb-all, rb-unique
                 if new_pattern["zone"][0] == "rb-all":
                     new_map = PixelMap(
                         pixels, blue_lights + red_lights, individual_pixels=True
                     )
+                elif new_pattern["zone"][0] == "rb-unique":
+                    new_map = PixelMap(
+                        pixels, blue_lights + red_lights, individual_pixels=True
+                    )
+                elif new_pattern["zone"][0] == "rb-toggle":
+                    toggle_group = [blue_lights, red_lights]
+                    new_map = PixelMap(pixels, toggle_group, individual_pixels=True)
                 elif new_pattern["zone"][0] == "white-all":
                     new_map = PixelMap(pixels, white_lights, individual_pixels=True)
                 elif new_pattern["zone"][0] == "all":
@@ -88,9 +100,9 @@ def lights(light_state): # pylint: disable=too-many-branches,too-many-statements
                 elif new_pattern["pattern"][0] == "chase":
                     pattern = Chase(new_map, speed=speed, color=tuple_color)
                 elif new_pattern["pattern"][0] == "colorcycle":
-                    if "colors" in new_pattern and len(new_pattern["colors"]) > 0:
+                    if "colors[]" in new_pattern and len(new_pattern["colors[]"]) > 0:
                         colors = []
-                        for hex_color in new_pattern["colors"]:
+                        for hex_color in new_pattern["colors[]"]:
                             hex_color = hex_color.lstrip("#")
                             colors.append(
                                 tuple(int(hex_color[i : i + 2], 16) for i in (0, 2, 4))
